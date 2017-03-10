9cd484d2d21b11db21585f1b683319530003a362
TINDRAJITH <indrajiththavasikumar@gmail.com>
2017-03-10 18:24:50 +0530

    Create pattern.c

---
+++ pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+		int i,j,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		printf("*");
+		printf("\n");
+	}
+	return 0;
+}
