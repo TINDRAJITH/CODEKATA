7cd84a759301c05c5ca18970216d67b2517f8ba1
TINDRAJITH <indrajiththavasikumar@gmail.com>
2017-03-16 13:06:42 +0530

    Update and rename revese.java to reverse.java

--- reverse.java
+++ reverse.java
@@ -1,14 +1,12 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
-	public static void main (String[] args) 
+	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
-		String a;
+		// your code goes 
+			String a;
 		int b;
 		Scanner s = new Scanner(System.in);
 		a = s.nextLine();
@@ -17,14 +15,18 @@ class Ideone
 		String k ="";
      	for(b=0;b<c.length;b++)
 		{
-			String word = c[b];
+			StringBuffer word = new StringBuffer(c[b]);
 			String j= "";
+			if((b+1)%2==0)
+			{
+				word =word.reverse();
+			}
 			for(int i = word.length()-1;i>=0;i--)
 			{
 			j=j+word.charAt(i);	
 			}
 		     k = k+j+" ";
 		}
-			System.out.println(k);
+System.out.println(k);
 	}
 }
