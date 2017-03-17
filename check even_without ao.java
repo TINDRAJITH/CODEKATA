c31a2e50c37b447d1a6a093e7b7f32200d4e7a70
TINDRAJITH <indrajiththavasikumar@gmail.com>
2017-03-17 13:22:39 +0530

    Create check even_without ao.java

---
+++ check even_without ao.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	int n;
+    Scanner in= new Scanner(System.in);
+    n=in.nextInt();
+ 
+    if((number & 1)==0){
+        System.out.println(+number+" is Even number");
+    }else{
+        System.out.println(+number+" is Odd Number");
+    }
+	}
+}
